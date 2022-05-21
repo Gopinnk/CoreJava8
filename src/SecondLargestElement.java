0abb2d87226d97c116e51158bd4afbd24fd81020
Gopi Vellireddy <Gopi.Vellireddy@coforge.com>
2022-05-22 00:05:43 +0530

    Java 8 feature added..

--- src/SecondLargestElement.java
+++ src/SecondLargestElement.java
@@ -1,3 +1,8 @@
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class SecondLargestElement {
 	
@@ -6,6 +11,11 @@ public class SecondLargestElement {
 		int arr[] = {10,20,25,45,29,30,50,76};
 		int len = arr.length;
 		int temp;
+		
+		List<Integer> list = Arrays.stream(arr).boxed().collect(Collectors.toList());
+		Optional<Integer> result = list.stream().sorted(Comparator.reverseOrder()).skip(2).findFirst();
+		System.out.println("USing Java 8 second higestNumber: " +result);
+		
 		
 		for(int i=0; i<len; i++) {
 			for(int j=i+1; j<len; j++) {
@@ -20,4 +30,6 @@ public class SecondLargestElement {
 		System.out.println("Largest Element: " +arr[len-3]);
 	}
 
+	
+	
 }
